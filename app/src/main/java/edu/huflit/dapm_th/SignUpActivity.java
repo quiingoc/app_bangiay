216c7ec9df27ffe1235904f849dc43558b3b4aea
QUI NGOC <nguyenquingoc03@gmail.com>
2023-09-22 23:00:58 +0700

    layout

---
+++ app/src/main/java/edu/huflit/dapm_th/SignUpActivity.java
@@ -0,0 +1,14 @@
+package edu.huflit.dapm_th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
