e68270689d7016b3260ccba188f2f3c650b6ed45
hienthanhnguyenngoc <hienthanhnguyenngoc@gmail.com>
2023-09-25 16:15:49 +0700

    add acctivity

---
+++ app/src/main/java/edu/huflit/dapm_th/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.huflit.dapm_th.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.huflit.dapm_th.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
